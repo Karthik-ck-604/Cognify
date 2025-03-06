3e18f5fb24e8fe904d70a7b118ef55d4b8a5d3e3
Karthikeyan C <karthikeyan610204@gmail.com>
2025-03-06 20:33:05 +0530

    init

---
+++ Task - 4/script.js
@@ -0,0 +1,3 @@
+function changeText() {
+    document.getElementById('change2').innerHTML = 'Add a button element to index.html Write JavaScript code in a tag or a separate file (script.js) to change the background color when the button is clicked.Test the functionality by clicking the button and observing the background color change.';
+}
